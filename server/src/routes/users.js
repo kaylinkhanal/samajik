c07eca209a217295782b0f35113c6d9951e96d8d
kbzcraft <ytkbzcraft@gmail.com>
2024-10-01 18:16:17 +0545

    just some try&catch

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -1,14 +1,12 @@
-
-const {Router} = require('express')
+const { Router } = require('express')
 const { registerUser, loginUser, findAllUser, findUserById, deleteUserById, updateUserById } = require('../controllers/users');
 const UserRoute = Router()
 
-UserRoute.post('/register', registerUser)
-UserRoute.post('/login',loginUser)
 UserRoute.get('/users', findAllUser)
-UserRoute.get('/users/:id',findUserById)
-UserRoute.delete('/users/:id',deleteUserById )
+UserRoute.get('/users/:id', findUserById)
+UserRoute.post('/register', registerUser)
+UserRoute.post('/login', loginUser)
 UserRoute.put('/users/:id', updateUserById)
-
+UserRoute.delete('/users/:id', deleteUserById)
 
 module.exports = UserRoute;
