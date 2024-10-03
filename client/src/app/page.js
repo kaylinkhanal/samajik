2d654ca2b9af6605eaea7fa3f48af086e9664743
Dr-vegapunk <Ishankarki7624@gmail.com>
2024-10-03 06:58:37 +0545

    added avatar

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,9 +1,19 @@
+import UserAvatar from '@/components/avatar'
+
 import SideBar from '@/components/sideBar'
 import React from 'react'
 
 const Home = () => {
   return (
-    <div><SideBar/></div>
+    <div><SideBar/>
+    <div className="absolute right-8 top-2 ">
+    <UserAvatar  />
+
+      
+    </div>
+    
+    
+    </div>
   )
 }
 
---
+++ client/src/components/avatar.js
@@ -0,0 +1,49 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+export default function UserAvatar() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" className="relative h-10 w-10 rounded-full">
+          <Avatar>
+            <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56" align="end" forceMount>
+        <DropdownMenuLabel className="font-normal">
+          <div className="flex flex-col space-y-1">
+            <p className="text-sm font-medium leading-none">shadcn</p>
+            <p className="text-xs leading-none text-muted-foreground">
+              m@example.com
+            </p>
+          </div>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          Profile
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          Change Password
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          Settings
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          Log out
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
